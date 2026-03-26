00517fd5003d4faa0d0b8ccf229d91669172457a
eduardopozenatto <235704056+eduardopozenatto@users.noreply.github.com>
2026-03-26 09:02:56 -0300

    feat: implemented an algorithm with private and public methods in user class to simulate a bank system

--- programacaoIII/exercise3.cpp
+++ programacaoIII/exercise3.cpp
@@ -1,20 +1,69 @@
 #include <iostream>
 #include <algorithm>
 #include <cctype>
+#include <stdexcept>
 using namespace std;
+
+class user {
+private:
+  int saldo;
+
+public:
+  void verSaldo(int ver);
+  void depositarValor(int deposito);
+  void sacarValor(int saque);
+
+};
+
+void user::sacarValor(int saque) {
+  int saqueTemporario = saldo - saque;
+
+  if (saqueTemporario < 0) {
+    cout << "você não pode sacar esse valor, pois é maior do que você tem na conta!";
+
+  } else {
+    saldo -= saque;
+    cout << "Parabéns! Você sacou R$" << saque << ". Deseja ver seu saldo? (s, n): ";
+
+  }
+};
 
 string transformarParaMinuscula(string operacao) {
   transform(operacao.begin(), operacao.end(), operacao.begin(), ::tolower);
   return operacao;
 }
 
+bool escolherOperacao(string operacao, user &usuario) {
+   if (operacao == "sacar") {
+    double valor;
+    cout << "insira o valor para sacar: ";
+    cin >> valor;
+    
+    usuario.sacarValor(valor);
+
+   } else if (operacao == "depositar") {
+    
+   } else if (operacao == "conferir") {
+    
+   } else if (operacao == "sair") {
+    return false;
+
+   } else {
+    cout << "Insira uma operação válida!";
+
+   } 
+};
+
 int main() {
   string operacao;
-  cout << "----SEU BANCO---- \nVocê pode sacar, depositar e conferir o seu saldo bancário.\nPara executar alguma ação, insira alguma das ações anteriores ou saia do banco (sair): ";
-  cin >> operacao;
-  
-  operacao = transformarParaMinuscula(operacao);
-  cout << operacao;
+  user usuario;
 
+  while (true) {
+    cout << "----SEU BANCO---- \nVocê pode sacar, depositar e conferir o seu saldo bancário\nPara executar alguma ação, insira alguma das ações anteriores ou saia do banco (sair): ";
+    cin >> operacao;
+    
+    operacao = transformarParaMinuscula(operacao);
 
+    escolherOperacao(operacao, usuario);
+  }
 }
