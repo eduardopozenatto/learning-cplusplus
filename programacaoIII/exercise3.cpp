f85c178a74c7455289965c19aceeac2c49a7784d
eduardopozenatto <235704056+eduardopozenatto@users.noreply.github.com>
2026-03-26 09:30:25 -0300

    feat(methods): finished the code algorithm and user methods to acess saldo in user class

--- programacaoIII/exercise3.cpp
+++ programacaoIII/exercise3.cpp
@@ -6,27 +6,74 @@ using namespace std;
 
 class user {
 private:
-  int saldo;
+  double saldo = 0;
 
 public:
-  void verSaldo(int ver);
+  void verSaldo();
   void depositarValor(int deposito);
   void sacarValor(int saque);
 
 };
 
+void refazer(user &usuario) {
+  char chose;
+  cin >> chose;
+  switch (chose) {
+    case 's':
+      usuario.verSaldo();
+      break;
+
+    case 'n':
+      cout << "saindo...";
+      break;
+    
+    default:
+      cout << "insira uma operação válida!";
+      break;
+
+  }
+}
+
+void user::verSaldo() {
+  cout << "Seu saldo disponível: " << saldo << endl;
+};
+
 void user::sacarValor(int saque) {
   int saqueTemporario = saldo - saque;
+  user usuario;
 
   if (saqueTemporario < 0) {
-    cout << "você não pode sacar esse valor, pois é maior do que você tem na conta!";
+    cout << "você não pode sacar esse valor, pois é maior do que você tem na conta!" << endl;
 
   } else {
     saldo -= saque;
     cout << "Parabéns! Você sacou R$" << saque << ". Deseja ver seu saldo? (s, n): ";
-
+    refazer(usuario);
   }
 };
+
+void user::depositarValor(int deposito) {
+  saldo += deposito;
+  char chose;
+  cout << "parabéns! Você depositou R$" << deposito << " em sua conta. Deseja ver seu saldo? (s, n): ";
+  cin >> chose;
+  
+  switch (chose) {
+    case 's':
+      verSaldo();
+      break;
+
+    case 'n':
+      cout << "saindo...";
+      break;
+    
+    default:
+      cout << "insira uma operação válida!";
+
+      break;
+  }
+  
+}
 
 string transformarParaMinuscula(string operacao) {
   transform(operacao.begin(), operacao.end(), operacao.begin(), ::tolower);
@@ -40,16 +87,28 @@ bool escolherOperacao(string operacao, user &usuario) {
     cin >> valor;
     
     usuario.sacarValor(valor);
+    
+    return true;
 
    } else if (operacao == "depositar") {
+    double deposito;
+    cout << "insira o valor para depositar: ";
+    cin >> deposito;
+    usuario.depositarValor(deposito);
+
+    return true;
     
    } else if (operacao == "conferir") {
+    usuario.verSaldo();
+    return true;
     
    } else if (operacao == "sair") {
+    cout << "saindo...";
     return false;
 
    } else {
-    cout << "Insira uma operação válida!";
+    cout << "Insira uma operação válida!" << endl;
+    return false;
 
    } 
 };
@@ -57,13 +116,16 @@ bool escolherOperacao(string operacao, user &usuario) {
 int main() {
   string operacao;
   user usuario;
+  bool loop = true;
 
-  while (true) {
-    cout << "----SEU BANCO---- \nVocê pode sacar, depositar e conferir o seu saldo bancário\nPara executar alguma ação, insira alguma das ações anteriores ou saia do banco (sair): ";
+   cout << "----SEU BANCO---- \nVocê pode sacar, depositar e conferir o seu saldo bancário" << endl;
+
+  while (loop) {
+    cout << "Para executar alguma ação, insira alguma das ações anteriores ou saia do banco (sair): ";
     cin >> operacao;
     
     operacao = transformarParaMinuscula(operacao);
 
-    escolherOperacao(operacao, usuario);
+    loop = escolherOperacao(operacao, usuario);
   }
 }
