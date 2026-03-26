30091205ab061c15a61bd89cbda242dc3594ab76
eduardopozenatto <235704056+eduardopozenatto@users.noreply.github.com>
2026-03-26 08:08:02 -0300

    feat(execption): added an cin error handling in exercise 4 algorithm

--- programacaoIII/exercise4.cpp
+++ programacaoIII/exercise4.cpp
@@ -1,20 +1,37 @@
 #include <iostream>
+#include <stdexcept>
 using namespace std;
 
+bool tratarErros() {
+
+
+  return true;
+}
 int main() {
+  cin.exceptions(ios::failbit | ios::badbit);
   cout << "Você pode inserir 10 valores dentro de um vetor. escolha com responsabilidade!";
   int vetor[10];
   string vetorMostrado;
-  string vetorAtualizado;
 
-  for (int i = 1; i <= 10; i++) {
-    int valor;
-    cout << "Insira um valor para inserir na posição " << i << " do vetor: ";
-    cin >> valor;
-    vetor[i] = valor;
-    vetorAtualizado = "[" + to_string(vetor[i]) + "]";
-    vetorMostrado.append(vetorAtualizado);
+  try {
+    for (int i = 1; i <= 10; i++) {
+      string vectorToString;
+      int valor;
+      cout << "Insira um valor para inserir na posição " << i << " do vetor: " << endl;
+      cin >> valor;
 
-    cout << "vetor atualizado: " << vetorMostrado << endl; 
-  };
-}
+      vetor[i] = valor;
+      vectorToString = "[" + to_string(vetor[i]) + "]";
+      vetorMostrado.append(vectorToString);
+
+      cout << "vetor atualizado: " << vetorMostrado << endl; 
+
+    };
+
+    cout << "seu vetor final: " << vetorMostrado << endl; 
+
+  } catch(runtime_error e) {
+    cout << "insira números válidos! erro: " << e.what(); 
+  }
+  
+  }
